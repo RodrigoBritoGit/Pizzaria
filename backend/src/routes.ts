7c0292de44ad7e898f75bc4dd8fb0141a0efe987
RodrigoBritoGit <Rodrigodossantosbrito@hotmail.com>
2024-04-20 12:31:14 -0300

    Método para cadastrar usuários

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,9 @@
 import { Router, Request, Response } from "express";
+import { CreateUserController } from "./controllers/User/CreateUserController";
 
 const router = Router();
 
-router.get("/teste", (req: Request, res: Response) => {
-  throw new Error('Alguma mensagem de erro!')
-});
+//-- ROTAS USER --
+router.post('/users', new CreateUserController().handle)
 
 export { router };
---
+++ backend/src/services/Users/CreateUserService.ts
@@ -0,0 +1,39 @@
+import prismaClient  from "../../prisma"
+
+interface UserRequest{
+    name: string,
+    email: string,
+    password: string
+}
+
+class CreateUserService {
+    async execute({name,email,password}: UserRequest){
+        // Verificar se foi enviado o email
+        if(!email){
+            throw new Error("Email incorreto")
+        }
+
+        // Verificar se esse email já está cadastrado na plataforma
+        const userAlreadExists = await prismaClient.user.findFirst({
+            where:{
+                email: email
+            }
+        })
+
+        if (userAlreadExists){
+            throw new Error("Usúario já cadastrado na plataforma")
+        }
+
+        const user = await prismaClient.user.create({
+            data: {
+                name: name,
+                email: email,
+                password: password
+            }
+        })
+
+        return user;
+    }
+}
+
+export {CreateUserService};
